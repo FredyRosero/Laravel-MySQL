9425445931611a3edbca59fe1b60e8f94fcba824
Fredy Rosero <faroseroc@unal.edu.co>
2021-04-08 04:50:50 -0500

    Modelo y controlador

--- laravel+mysql/routes/api.php
+++ laravel+mysql/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Animal;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/CrearAnimal', function() {
+    Animal::create(['nombre'=>'tom','f_nacimiento'=>'2021-12-22']);
+});
+
+Route::get('/hola', function() {
+    return "mundo!";
+});
+
+Route::get('/info', function() {
+    phpinfo();
+});
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
